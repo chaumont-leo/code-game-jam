6d3861a8c3aa6f636c46eb32774a6dc9e770820a
E224340Y <thomas.ambroise@etu.univ-nantes.fr>
2025-01-24 16:37:10 +0100

    update

--- core/src/main/kotlin/fr/github/sahrchivage/Main.kt
+++ core/src/main/kotlin/fr/github/sahrchivage/Main.kt
@@ -17,48 +17,10 @@ import fr.github.sahrchivage.utils.getInternalTexture
 
 class Main : Game() {
     lateinit var world: World
-    lateinit var camera: OrthographicCamera
-    lateinit var viewport: Viewport
-    lateinit var batch: SpriteBatch
-    // lateinit var player: Player
 
     override fun create() {
-        camera = OrthographicCamera(
-            Gdx.graphics.width.toFloat(),
-            Gdx.graphics.height.toFloat(),
-        ).also { it.setToOrtho(false,
-                Gdx.graphics.width.toFloat(),
-                Gdx.graphics.height.toFloat()) }
-        println(Gdx.graphics.width)
-        println(Gdx.graphics.height)
-        camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0f)
-        viewport = FitViewport(Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat(), camera)
-        batch = SpriteBatch()
-        world = World(Vector2(0f, -10f), true)
-
-        camera.update()
         setScreen(TitleScreen())
         screen.show()
-    }
-
-    override fun render() {
-        Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        screen.render(Gdx.graphics.deltaTime)
-
-        camera.update()
-
-        batch.projectionMatrix = camera.combined
-        batch.begin()
-        batch.draw(getInternalTexture("bg.png"), 0f, 0f)
-        batch.end()
-    }
-
-    override fun resize(width: Int, height: Int) {
-        viewport.update(width, height)
-    }
-
-    override fun dispose() {
     }
 
     fun startGame() {
