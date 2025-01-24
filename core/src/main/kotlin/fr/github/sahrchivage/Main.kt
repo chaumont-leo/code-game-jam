a91086081aecd682d270db6c5fbaacef2e52f25b
E224340Y <thomas.ambroise@etu.univ-nantes.fr>
2025-01-24 17:24:45 +0100

    encore une update

--- core/src/main/kotlin/fr/github/sahrchivage/Main.kt
+++ core/src/main/kotlin/fr/github/sahrchivage/Main.kt
@@ -2,25 +2,46 @@ package fr.github.sahrchivage
 
 import com.badlogic.gdx.Game
 import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.Input
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.math.Rectangle
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.World
-import com.badlogic.gdx.utils.viewport.FitViewport
-import com.badlogic.gdx.utils.viewport.Viewport
+import fr.github.sahrchivage.models.Player
 import fr.github.sahrchivage.screens.GameScreen
-import fr.github.sahrchivage.screens.TitleScreen
-import fr.github.sahrchivage.utils.getInternalTexture
 
 class Main : Game() {
     lateinit var world: World
+    lateinit var renderer: ShapeRenderer
+    lateinit var player: Player
+    var accumulator: Float = 0f
 
     override fun create() {
-        setScreen(TitleScreen())
-        screen.show()
+        world = World(Vector2(0F, 0F), true)
+        renderer = ShapeRenderer()
+        player = Player()
+    }
+
+    override fun render() {
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+        player.handleInput()
+
+
+        // fixed time step
+        // max frame time to avoid spiral of death (on slow devices)
+        val frameTime = Gdx.graphics.deltaTime.coerceAtMost(0.25f)
+        accumulator += frameTime
+        while (accumulator >= TIME_STEP) {
+            world.step(TIME_STEP, VELOCITY_ITERATIONS, POSITION_ITERATIONS)
+            accumulator -= TIME_STEP
+        }
+
+        renderer.begin(ShapeRenderer.ShapeType.Filled)
+        renderer.color = Color.GREEN
+        renderer.rect(player.body.position.x, player.body.position.y, 50f, 50f)
+        renderer.end()
     }
 
     fun startGame() {
--- core/src/main/kotlin/fr/github/sahrchivage/models/Player.kt
+++ core/src/main/kotlin/fr/github/sahrchivage/models/Player.kt
@@ -1,11 +1,13 @@
 package fr.github.sahrchivage.models
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
+import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.*
 import fr.github.sahrchivage.Main
 
 class Player : BaseModel()
 {
-
     init {
         val bodyDef = BodyDef().apply { type = BodyDef.BodyType.DynamicBody }
         this.body = Main.getMain().world.createBody(bodyDef)
@@ -16,6 +18,12 @@ class Player : BaseModel()
         fixtureDef.friction = 0.4f;
         fixtureDef.restitution = 0.6f;
         this.fixture = this.body.createFixture(fixtureDef)
+    }
+
+    fun handleInput() {
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            this.body.applyForce(Vector2(0f, -9.81f), body.position, true)
+        }
     }
 
     override fun dispose() {
